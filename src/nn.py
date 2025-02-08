691a31e058ac46a1ae58ef2f64f2179bdd4ae4cf
tinuademargaret <summittinuade@gmail.com>
2025-02-08 20:31:38 +0000

    residual block

--- src/nn.py
+++ src/nn.py
@@ -2,6 +2,7 @@
 Customized nn modules
 """
 
+import torch
 import torch.nn as nn
 
 import torch.nn.functional as F
@@ -13,6 +14,39 @@ class GroupNorm32(nn.GroupNorm):
 
     def forward(self, x):
         return super().forward(x.float()).type(x.dtype)
+
+
+class CheckpointFunction(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, run_function, length, *args):
+        # store function input so we can run them again during backward pass
+        ctx.run_function = run_function
+        ctx.input_tensors = list(args[:length])
+        ctx.input_params = list(args[length:])
+        with torch.no_grad():
+            output_tensors = ctx.run_function(*ctx.input_tensors)
+        return output_tensors
+
+    @staticmethod
+    def backward(ctx, *output_grads):
+        # remove them from computational graph and set requires_grad to true so that their activations are stored
+        ctx.input_tensors = [x.detach().requires_grad(True) for x in ctx.input_tensors]
+        with torch.enable_grad():
+            shallow_copies = [x.view_as(x) for x in ctx.input_tensors]
+            output_tensors = ctx.run_function(*shallow_copies)
+        # calculate gradient of input wrt their output
+        input_grads = torch.autograd.grad(
+            output_tensors,
+            ctx.input_tensors + ctx.input_params,
+            output_grads,
+            allow_unused=True,  # allows us to specify iputs that were not used when calculating the output
+        )
+        # clean up
+        del ctx.input_tensors
+        del ctx.input_params
+        del output_tensors
+        return (None, None) + input_grads
 
 
 def conv_nd(dim, *args, **kwargs):
@@ -33,3 +67,23 @@ def normalization(channels):
     GroupNorm -> Divides the channels into groups and normalizes within each group.
     """
     return GroupNorm32(32, channels)
+
+
+def linear(*args, **kwargs):
+    """Returns a linear module"""
+    return nn.Linear(*args, **kwargs)
+
+
+def zero_module(module):
+    for param in module.parametets():
+        param.detach().zero_()
+    return module
+
+
+def checkpoint(func, inputs, params, use_checkpoint):
+
+    if use_checkpoint:
+        args = tuple(inputs) + tuple(params)
+        return CheckpointFunction.apply(func, len(inputs), *args)
+    else:
+        return func(*inputs)
--- src/unet.py
+++ src/unet.py
@@ -1,4 +1,8 @@
+import torch
 import torch.nn as nn
+from abc import ABC, abstractmethod
+
+from .nn import conv_nd, normalization, linear, zero_module
 
 """
 Unet model architecture
@@ -38,6 +42,133 @@ Unet Class
 """
 
 
+class TimeBlock(nn.Module):
+
+    @abstractmethod
+    def forward(self, x, emb):
+        """"""
+
+
+class TimeEmbedSequential(nn.Sequential, TimeBlock):
+
+    def forward(self, x, emb):
+        # basically for layer in nn.Sequential, nn.Sequential is also a nn.Module type
+        for layer in self:
+            if isinstance(layer, TimeBlock):
+                x = layer(x, emb)
+            else:
+                x = layer(x)
+        return x
+
+
+class ResBlock(TimeBlock):
+    """
+    residual path -> h=in_layers(x)→emb_layers(emb)→out_layers(h)
+    in_layer - norm -> silu -> conv ->
+    emb_layer - silu -> linear
+    out_layer - norm -> silu -> dropout -> conv -> zero_module
+
+    skip_connection -> conv(x) + h
+    """
+
+    def __init__(
+        self,
+        channels,
+        emb_dim,
+        dims,
+        out_channels,
+        use_scale_shift_norm,
+        dropout,
+        use_conv,
+        use_checkpoint,
+    ):
+        super().__init__()
+        self.channels = channels
+        self.emb_dim = emb_dim
+        self.out_channels = out_channels
+        self.use_scale_shift_norm = use_scale_shift_norm
+        self.dropout = dropout
+        self.use_conv = use_conv
+        self.use_checkpoint = use_checkpoint
+
+        self.in_layer = nn.Sequential(
+            normalization(self.channels),
+            nn.SiLU(),
+            conv_nd(dims, channels, self.out_channels, 3, padding=1),
+        )
+
+        self.emb_layer = nn.Sequential(
+            nn.SiLU(),
+            linear(
+                self.emb_dim,
+                (
+                    2 * self.out_channels
+                    if self.use_scale_shift_norm
+                    else self.out_channels
+                ),
+            ),
+        )
+
+        self.out_layer = nn.Sequential(
+            normalization(self.out_channels),
+            nn.SiLU,
+            nn.Dropout(p=dropout),
+            zero_module(
+                conv_nd(dims, self.out_channels, self.out_channels, 3, padding=1)
+            ),
+        )
+
+        if self.out_channels == channels:
+            self.skip_connection = nn.Identity()
+        elif use_conv:
+            self.skip_connection = conv_nd(
+                dims, channels, self.out_channels, 3, padding=1
+            )
+        else:
+            self.skip_connection = conv_nd(dims, channels, self.out_channels, 1)
+
+    def forward(self):
+        return
+
+    def _forward(self, x, emb):
+        """
+        h = x's resid path
+        emb_out = emb's resid_path
+        resid_path_out = out(h + emmb)
+
+        skip_connection(x) + resid_path_out
+        """
+        h = self.in_layer(x)
+        emb_out = self.emb_layer(emb).type(h.dtype)
+
+        while len(emb_out.shape) < len(h.shape):
+            emb_out = emb_out[..., None]
+
+        if self.use_scale_shift_norm:
+            out_norm, out_rest = self.out_layers[0], self.out_layers[1:]
+            scale, shift = torch.chunk(emb_out, 2, dim=1)
+            h = out_norm(h) * (1 + scale) + shift
+            h = out_rest(h)
+        else:
+            h = h + emb_out
+            h = self.out_layer(h)
+        return self.skip_conection(x) + h
+
+
+class AttentionBlock:
+    """
+    QK, KV circuits
+
+    create  attention pattern i.e a weighing of how much a destination pixel is important to a source pixel
+    move the information from a destination token to a source token
+
+    An attention bl
+
+    """
+
+    pass
+
+
 class UNetModel(nn.Module):
     """
     Each Relu(3x3conv) block = Resblock + Attnblock
@@ -71,6 +202,23 @@ class UNetModel(nn.Module):
         super().__init()
 
         """
+        Params
+            image_size -> 64
+            num_channels -> internal channel for unet, img_channel is 3 as per (RGB channels)
+            learn_sigma -> whether to learn variation or not
+            class-cond -> using classes to condition, we use tempral conditioning
+            use_chckpt -> optmization technique (memory for time) not to store all the activations during a fwd pass, but durig bckwd they'll be recomputed
+            attn_resolns -> 
+            num_heads -> attn layer
+            use_scale_shift_norm -> determines how to condition the images with the timestep
+            dropout ->
+            attention_ds -> when to start including attention layers in unet
+
+        time embed layer(simple mlp)
+            linear
+            SILU
+            linear
+
         Input Block
             in_blcks = [conv layer] 
             for input level
