732350b1aa7677c0a67723828d0f54daf2a725a8
tinuademargaret <summittinuade@gmail.com>
2025-02-13 15:19:28 +0000

    adds output blocks

--- src/unet.py
+++ src/unet.py
@@ -313,8 +313,13 @@ class UNetModel(nn.Module):
         use_scale_shift_norm=False,
         use_checkpoint=False,
         num_heads=1,
+        num_heads_upsample=-1,
     ):
         super().__init()
+
+        if num_heads_upsample == -1:
+            num_heads_upsample = num_heads
+
         self.in_channels = in_channels
         self.multi_channels = multi_channels
         self.num_res_block = num_res_blocks
@@ -325,6 +330,7 @@ class UNetModel(nn.Module):
         self.use_scale_shift_norm = use_scale_shift_norm
         self.use_checkpoint = use_checkpoint
         self.num_heads = num_heads
+        self.num_heads_upsample = num_heads_upsample
 
         """
         Params
@@ -389,34 +395,38 @@ class UNetModel(nn.Module):
                 )
             ]
         )
+        input_block_chans = [model_channels]
         ds = 1
         ch = self.model_channels
         for level, mult in enumerate(self.multi_channels):
             for _ in range(self.num_res_blocks):
                 layers = [
                     ResBlock(
-                        self.model_channels,
+                        ch,
                         time_embed_dim,
                         self.dropout,
-                        mult * self.model_channels,
+                        dims=dims,
+                        out_channels=mult * self.model_channels,
                         use_conv=self.conv_resample,
                         use_scale_shift_norm=self.use_scale_shift_norm,
                     )
                 ]
-            ch *= mult
-            if ds in self.attention_resolutions:
-                layers.append(
-                    AttentionBlock(
-                        ch,
-                        use_checkpoint=self.use_checkpoint,
-                        num_heads=self.num_heads,
+                ch *= mult
+                if ds in self.attention_resolutions:
+                    layers.append(
+                        AttentionBlock(
+                            ch,
+                            use_checkpoint=self.use_checkpoint,
+                            num_heads=self.num_heads,
+                        )
                     )
-                )
-            self.input_blocks.append(TimeEmbedSequential(*layers))
+                self.input_blocks.append(TimeEmbedSequential(*layers))
+                input_block_chans.append(ch)
         if level != len(self.multi_channels) - 1:
             self.input_blocks.append(
                 TimeEmbedSequential(Downsample(ch, self.conv_resample, dims=dims))
             )
+            input_block_chans.append(ch)
             ds *= 2
 
         self.middle_block = TimeEmbedSequential(
@@ -438,6 +448,38 @@ class UNetModel(nn.Module):
                 use_scale_shift_norm=use_scale_shift_norm,
             ),
         )
+
+        self.output_block = nn.ModuleList([])
+        for level, mult in list(enumerate(self.multi_channels))[::-1]:
+            for i in range(num_res_blocks + 1):
+                layers = []
+                layers.append(
+                    ResBlock(
+                        ch + input_block_chans.pop(),
+                        time_embed_dim,
+                        dropout,
+                        dims=dims,
+                        out_channels=mult * self.model_channels,
+                        use_checkpoint=use_checkpoint,
+                        use_scale_shift_norm=use_scale_shift_norm,
+                    )
+                )
+                ch = mult * model_channels
+                if ds in self.attention_resolutions:
+                    layers.append(
+                        AttentionBlock(
+                            ch,
+                            num_heads=num_heads_upsample,
+                            use_checkpoint=use_checkpoint,
+                        )
+                    )
+                self.output_block.append(TimeEmbedSequential(*layers))
+
+            if level and i == num_res_blocks:
+                self.output_block.append(
+                    TimeEmbedSequential(Upsample(ch, conv_resample, dims=dims))
+                )
+                ds //= 2
 
     def forward(self):
         """
