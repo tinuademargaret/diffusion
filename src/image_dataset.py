b190fd182d4130d4c73eab3f808f7f202f55b32c
tinuademargaret <summittinuade@gmail.com>
2025-02-17 23:28:56 +0000

    list file recursively

--- src/image_dataset.py
+++ src/image_dataset.py
@@ -4,6 +4,19 @@ import numpy as np
 from torch.utils.data import Dataset
 
 
+def _list_image_files_recursively(data_dir):
+    results = []
+
+    for entry in sorted(bf.listdir(data_dir)):
+        full_path = bf.join(data_dir, entry)
+        ext = entry.split(".")[-1]
+        if "." in entry and ext in ["jpg", "jpeg", "png", "gif"]:
+            results.append(full_path)
+        elif bf.isdir(full_path):
+            results.extend(_list_image_files_recursively(full_path))
+    return results
+
+
 class ImageDataset(Dataset):
 
     def __init__(self, resolution, image_paths):
